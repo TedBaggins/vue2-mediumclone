acb796f7d5aabe0c30a873f63e7efd2fd28f2d00
TedBaggins <tedbagginsrb@gmail.com>
2021-02-17 13:18:06 +0300

    login page, register page

---
+++ src/App.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        Medium clone
+        <router-view />
+    </div>
+</template>
---
+++ src/store/modules/auth.js
@@ -0,0 +1,98 @@
+import authApi from '@/api/auth';
+import {setItem} from '@/helpers/persistanceStorage';
+
+const state = {
+    isSubmitting: false,
+    currentUser: null,
+    validationErrors: null,
+    isLoggedIn: null
+};
+
+export const mutationTypes = {
+    registerStart: '[auth] registerStart',
+    registerSuccess: '[auth] registerSuccess',
+    registerFailure: '[auth] registerFailure',
+
+    loginStart: '[auth] loginStart',
+    loginSuccess: '[auth] loginSuccess',
+    loginFailure: '[auth] loginFailure'
+}
+
+// в мутациях код должен быть чистым, тут только меняется состояние
+const mutations = {
+    [mutationTypes.registerStart](state) {
+        state.isSubmitting = true;
+        state.validationErrors = null;
+    },
+    [mutationTypes.registerSuccess](state, payload) {
+        state.isSubmitting = false;
+        state.currentUser = payload;
+        state.isLoggedIn = true;
+    },
+    [mutationTypes.registerFailure](state, payload) {
+        state.isSubmitting = false;
+        state.validationErrors = payload;
+    },
+
+    [mutationTypes.loginStart](state) {
+        state.isSubmitting = true;
+        state.validationErrors = null;
+    },
+    [mutationTypes.loginSuccess](state, payload) {
+        state.isSubmitting = false;
+        state.currentUser = payload;
+        state.isLoggedIn = true;
+    },
+    [mutationTypes.loginFailure](state, payload) {
+        state.isSubmitting = false;
+        state.validationErrors = payload;
+    }
+};
+
+export const actionTypes = {
+    register: '[auth] register',
+    login: '[auth] login'
+}
+
+// экшены - хорошее место, чтобы делать сайд-эффекты
+const actions = {
+    [actionTypes.register](context, credentials) {
+        // желательно у каждого экшена возвращать промис, даже если он пока не нужен
+        return new Promise((resolve) => {
+            context.commit(mutationTypes.registerStart);
+            authApi
+                .register(credentials)
+                .then((response) => {
+                    context.commit(mutationTypes.registerSuccess, response.data.user);
+                    setItem('accessToken', response.data.user.token);
+                    resolve(response.data.user);
+                })
+                .catch((result) => {
+                    context.commit(mutationTypes.registerFailure, result.response.data.errors);
+                });
+        });
+    },
+
+    [actionTypes.login](context, credentials) {
+        // желательно у каждого экшена возвращать промис, даже если он пока не нужен
+        return new Promise((resolve) => {
+            context.commit(mutationTypes.loginStart);
+            authApi
+                .login(credentials)
+                .then((response) => {
+                    context.commit(mutationTypes.loginSuccess, response.data.user);
+                    setItem('accessToken', response.data.user.token);
+                    resolve(response.data.user);
+                })
+                .catch((result) => {
+                    context.commit(mutationTypes.loginFailure, result.response.data.errors);
+                });
+        });
+    },
+};
+
+export default {
+    state,
+    mutations,
+    actions,
+};
