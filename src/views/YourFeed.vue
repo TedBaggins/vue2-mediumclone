65996053978bd9a5e1f739fb9685e2287caec61c
TedBaggins <tedbagginsrb@gmail.com>
2021-02-18 17:20:39 +0300

    add feed toggler

--- src/views/YourFeed.vue
+++ src/views/YourFeed.vue
@@ -4,6 +4,7 @@
         <div class="container page">
             <div class="row">
                 <div class="col-md-9">
+                    <McvFeedToggler/>
                     <McvFeed :api-url="apiUrl"/>
                 </div>
                 <div class="col-md-3">
@@ -18,13 +19,15 @@
     import McvFeed from '@/components/Feed';
     import McvPopularTags from '@/components/PopularTags';
     import McvBanner from '@/components/Banner';
+    import McvFeedToggler from '@/components/FeedToggler';
 
     export default {
         name: 'McvYourFeed',
         components: {
             McvPopularTags,
             McvFeed,
-            McvBanner
+            McvBanner,
+            McvFeedToggler
         },
         data() {
             return {
